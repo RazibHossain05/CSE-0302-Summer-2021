8558f24cb1d461849f75db9e213dc32e7d431e4c
Md Razib Hossain <86564965+RazibHossain05@users.noreply.github.com>
2021-08-07 23:31:09 +0600

    Add files via upload

---
+++ Mid Assignment/Assignment 1/assignment 1.c
@@ -0,0 +1,47 @@
+/*Md Razib Hossain
+id:UG02-49-18-005
+*/
+#include<stdio.h
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+    FILE *input;
+    FILE *output;
+    string s;
+    char c;
+    input = fopen("input.txt","r");
+    output = fopen("output.txt","w");
+
+    while(!feof(input))
+    {
+        c = fgetc(input);
+        s+=c;
+    }
+    cout<<s;
+    cout<<endl;
+
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == '/' && s[i+1] == '/')
+            while(s[i] != '\n') i++;
+
+        else if(s[i] == '*')
+            while(s[i] != '/') i++;
+
+        else if(s[i]!=32 && s[i]!=9 && s[i]!='\n' && s[i]!='/' && s[i]!='*')
+        {
+            if(s[i-1] == '}') continue;
+           cout<<s[i];
+           fputc(s[i],output);
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+    return 0;
+}
